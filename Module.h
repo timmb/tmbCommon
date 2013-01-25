009fd9844b02bbdb6471001f8a046ab9533da61e
Tim <tim@timmb.com>
2013-01-25 00:18:26 +0000

    midi note name, deg to rad, pi

--- Module.h
+++ Module.h
@@ -5,7 +5,7 @@
 //
 //
 
-/// A generic interface for classes that use the openframeworks/cinder style setup/update/draw mechanics
+/// A generic interface for classes that use the openframeworks/cinder style update/draw mechanics
 
 #pragma once
 
@@ -14,7 +14,6 @@ namespace tmb
 	class Module
 	{
 	public:
-		virtual void setup() {}
 		virtual void update(float dt, float elapsedTime) {}
 		virtual void draw() {}
 	};
--- tmbUtils.h
+++ tmbUtils.h
@@ -8,19 +8,45 @@
 #pragma once
 
 #include <string>
-#include <stringstream>
+#include <sstream>
+
+#define FILESTRING____ (std::string(__FILE__).substr(std::string(__FILE__).find_last_of('/')))
+#define LOG(str) std::cout << "LOG: " << FILESTRING____ << ':' << __LINE__ << ' ' << __func__ << ": " << str << std::endl;
+#define ERR(str) std::cerr << "ERR: " << FILESTRING____ << ':' << __LINE__ << ' ' << __func__ << ": " << str << std::endl;
 
 namespace tmb
 {
+	/// Convert a streamable type to a string
 	template <typename T>
 	std::string toString(T const& x)
 	{
 		std::stringstream s;
+		s << std::fixed;
 		s << x;
 		return s.str();
 	}
 	
+#ifndef PI
+	static const double PI = (3.14159265358979323846264338327950288);
+#endif
 	
 	
+	static std::string midiNoteName(int midiNote)
+	{
+		if (midiNote<0 || midiNote>=128)
+			return "?";
+
+		const std::string notes[12] = { "C","C#","D","D#","E","F","F#","G","G#","A","A#","B" };
+		const std::string octaves[11] =
+		{ "0","1","2","3","4","5","6","7","8","9", "10" };
+		return notes[midiNote%12] + octaves[midiNote/12];
+	}
+
+
+	template <typename T>
+	T toDegrees(T const& radians)
+{
+	return radians * 360. / (2. * PI);
+}
 	
 }
